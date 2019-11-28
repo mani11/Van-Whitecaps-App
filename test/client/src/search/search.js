044b816d54b91c43d628f54d1f183aa0e222e690
Manleen Bhatia <manleenbhatia@Manleens-MacBook-Pro.local>
2019-11-27 16:37:25 -0800

    refatoring the code in search.js

--- test/client/src/search/search.js
+++ test/client/src/search/search.js
@@ -10,18 +10,11 @@ const applySearch = (key, playerData) => {
   return playerData.filter((player) => {
     const { number, name, nationality, position, height, weight, dob, birthplace } = player;
     key = key.toLowerCase();
-    if (name.toLowerCase().includes(key) ||
-      nationality.toLowerCase().includes(key) ||
-      position.toLowerCase().includes(key) ||
-      dob.toLowerCase().includes(key) ||
-      birthplace.toLowerCase().includes(key) ||
-      weight.toString().includes(key) ||
-      height.toString().includes(key) ||
-      number.toString().includes(key)
-    ) {
-      return true;
-    }
-    return false;
+    return (name.toLowerCase().includes(key) || nationality.toLowerCase().includes(key) 
+          || position.toLowerCase().includes(key) || dob.toLowerCase().includes(key) 
+          || birthplace.toLowerCase().includes(key) || weight.toString().includes(key) 
+          || height.toString().includes(key) || number.toString().includes(key)
+    ) 
   })
 }
 
